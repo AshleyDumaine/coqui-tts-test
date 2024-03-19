c632051cdfa3388a3bebf9559edcf9e5be527f47
AshleyDumaine <ashley.dumaine@gmail.com>
2024-03-18 20:44:56 -0400

    set speaker idx for vctk models

--- generate-tts-samples.sh
+++ generate-tts-samples.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set euxo pipefail
+set -euo pipefail
 
 OUTPUT_DIR=audio-samples
 INPUT_TEXT="$(cat sample.txt)"
@@ -11,5 +11,12 @@ for model in $en_models; do
     # create subdir if needed
     mkdir -p "${OUTPUT_DIR}/$(echo ${model} |  cut -d/ -f1)"
     # generate audio file
-    tts --model_name tts_models/en/$model --out_path "${OUTPUT_DIR}/${model}-sample.wav" --use_cuda True --text "${INPUT_TEXT}"
+    if [[ "$model" == "vctk"* ]]; then
+	# need a speaker index for multi-speaker models
+	SPEAKER_IDX=$(tts --model_name "tts_models/en/${model}" --list_speaker_idxs | awk 'END {print $3}' | tr -d "'" | tr -d ":")
+	# vctk/fast_pitch complains about cuda devices so just run these on the cpu
+	tts --model_name tts_models/en/$model --out_path "${OUTPUT_DIR}/${model}-${SPEAKER_IDX}-sample.wav" --text "${INPUT_TEXT}" --speaker_idx "${SPEAKER_IDX}"
+    else
+        tts --model_name tts_models/en/$model --out_path "${OUTPUT_DIR}/${model}-sample.wav" --use_cuda True --text "${INPUT_TEXT}"
+    fi
 done
